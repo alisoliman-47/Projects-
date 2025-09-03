5e50f9db1f22c0a1f3431e39409ae2e9aa9f82dd
Ali <aliamrsoliman@gmail.com>
2025-09-02 22:02:46 -0400

    Continued to setup the first product on the home page, which is the lean startup

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -8,12 +8,13 @@ function Home() {
       <div className='home__container'>
         <img 
           className='home__image'
-          src="https://media.licdn.com/dms/image/v2/C5112AQE9cMJ6jKIZtg/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1585711743157?e=2147483647&v=beta&t=I0oxluozH5wiTroJpLP9lSumXZn53jR3RTrYD2HM0ME"
+          src="https://www.shutterstock.com/image-photo/buenos-aires-argentina-08142023-logo-260nw-2347424263.jpg"
           alt=""
         />
       </div>
 
       <div className='home__row'>
+        <Product/>
         <Product/>
       </div>
 
